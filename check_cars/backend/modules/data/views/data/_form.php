6dc91c07850671e86f71fbd25305b0649a2329dc
kritsada <kritsada.swe@gmail.com>
2016-08-05 23:41:32 +0700

    add widget

--- check_cars/backend/modules/data/views/data/_form.php
+++ check_cars/backend/modules/data/views/data/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\widgets\DateTimePicker;
 /* @var $this yii\web\View */
 /* @var $model backend\modules\data\models\Data */
 /* @var $form yii\widgets\ActiveForm */
@@ -14,8 +14,20 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'license_plate')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'date_time')->textInput() ?>
+    <!--?= $form->field($model, 'date_time')->textInput() ?-->
+    <?php echo DateTimePicker::widget([
+      'model' => $model,
+        'attribute' => 'date_time',
+    'name' => 'datetime_10',
+    'options' => ['placeholder' => 'Select operating time ...'],
+    //'convertFormat' => true,
+    'pluginOptions' => [
 
+        //'format' => 'Y-M-d H:i:s ',
+        //'startDate' => '01-Mar-2014 12:00 AM',
+        'todayHighlight' => true
+    ]
+]);?>
     <?= $form->field($model, 'status_id')->textInput() ?>
 
     <div class="form-group">
--- check_cars/backend/modules/data/views/data/index.php
+++ check_cars/backend/modules/data/views/data/index.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
-
+use kartik\widgets\SwitchInput;
 /* @var $this yii\web\View */
 /* @var $searchModel backend\modules\data\models\DataSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -16,8 +16,19 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Data', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('เข้า', ['create'], ['class' => 'btn btn-success','value'=> 0]) ?>
+        <?= Html::a('ออก', ['create'], ['class' => 'btn btn-danger','value'=> 1]) ?>
+
     </p>
+    <?php echo SwitchInput::widget([
+    'name'=>'status_id',
+    'pluginOptions'=>[
+        'handleWidth'=>60,
+        'onText'=>'in',
+        'offText'=>'out'
+    ]
+]); ?>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
